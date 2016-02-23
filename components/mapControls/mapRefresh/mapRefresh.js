5bbb7c39cc79633c47396615c00119f259e00ac1
Will Robinson <willdrob@gmail.com>
2016-02-22 19:37:32 -0500

    Updated map controls

--- components/mapControls/mapRefresh/mapRefresh.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp.mapControls')
-    .controller('mapRefreshCtrl', ['$scope', function ($scope) {
-        $scope.countdownTime = 30;
-		$scope.$on('setCountdownTime', function (event, time) { 
-			$scope.countdownTime = time;
-		});
-    }]);
--- components/mapControls/mapTitle/mapTitle.js
+++
@@ -1,4 +0,0 @@
-angular.module('myApp.mapControls')
-    .controller('mapTitleCtrl', ['$scope','$routeParams', function ($scope, $routeParams) {
-        $scope.city = $routeParams.city;
-    }]);
